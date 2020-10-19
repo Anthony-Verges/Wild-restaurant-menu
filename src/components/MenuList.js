07742f72ff9c95b69ae024817dccfe82050b35c3
MacBook Pro Anthony <macbookproanthony@MBP-de-Anthony.lan>
2020-10-19 11:42:37 +0200

    resolve bug on component MenuList

--- src/components/MenuList.js
+++ src/components/MenuList.js
@@ -4,8 +4,8 @@ import MenuItem from './MenuItem';
 const MenuList = ({ menuItems, foodItems }) => {
     return (
         <div>
-            { menuItems.map(item => <MenuItem itemInfo={item} key={item.itemName} /> ) }
-            {foodItems.map((item)=> <MenuItem itemName={item} description={item} foodImage={item} price={item} isFavorite={item} /> ) }
+            { menuItems.map(item => <MenuItem itemInfo={item} key={item.itemName} itemName={item} description={item} foodImage={item} price={item} isFavorite={item} /> ) }
+            
         </div>
     )
 }
